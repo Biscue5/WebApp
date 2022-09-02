f92e25c6b3150dfc2d0c92efaaee2b1cabc8192f
biscue <slimeliment@gmail.com>
2022-09-02 18:49:42 +0900

    first one

--- text_generation.py
+++ text_generation.py
@@ -1,3 +1,21 @@
 import torch
+import pandas as pd
+import streamlit as st
 
-ss
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model_name = 'gpt2'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+
+input_txt = st.text_input('type some text..')
+input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
+iterations = []
+n_steps = st.number_input('the length of sentence', 0, 100, 0)
+choice_per_step = 5
+
+input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
+output = model.generate(input_ids, max_new_tokens=n_steps, do_sample=False)
+
+st.text_area('generated text: {}'.format(tokenizer.decode(output[0])))
