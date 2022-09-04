8b67cc0b479e1e7eb1d6627ba5593f9736771d0f
Tomoya Yoshida <tomoya@Tomoyas-MacBook-Air.local>
2022-09-04 18:35:16 +0900

    available

--- text_generation.py
+++ text_generation.py
@@ -4,18 +4,27 @@ import streamlit as st
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model_name = 'gpt2'
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+def text_generator(input_txt, n_steps):
 
-input_txt = st.text_input('type some text..')
-input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
-iterations = []
-n_steps = st.number_input('the length of sentence', 0, 100, 0)
-choice_per_step = 5
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model_name = 'gpt2'
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 
-input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
-output = model.generate(input_ids, max_new_tokens=n_steps, do_sample=False)
+    input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
+    output = model.generate(input_ids, max_new_tokens=n_steps, do_sample=False)
 
-st.text_area('generated text: {}'.format(tokenizer.decode(output[0])))
+    return tokenizer.decode(output[0])
+
+
+st.title('Text Generator')
+
+input_txt = st.text_input('type a couple of words')
+sentence_l = st.number_input('the length of sentence', 0, 20, 0)
+
+button = st.button('Generate')
+
+st.write('Output: \n')
+
+if button:
+    st.write(text_generator(input_txt, sentence_l))
